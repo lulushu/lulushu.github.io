818bfb6455b8883ed092658f81126f1edde9fe46
Chang weonseok <iampoem315@gmail.com>
2016-09-17 21:06:19 +0900

    first commit
    first commit

---
+++ js/vrlist_object2.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+
+    var win = window; // eslint-disable-line no-undef
+    var jq = win.jQuery;
+    var appendContent = true;
+    var vrlist = [ '','브레인스토밍룸', '창의공학설계스튜디오'];
+    
+    function generateContent() {
+        jq('.scrollpanel').each(function (idx, el) {
+            for (var i = 1; i < vrlist.length; i += 1) {
+                jq(el).append('<div class="item">'
+                                +'<a target="demo" href="vr2_'
+                                +i+'.html">'
+                                +vrlist[i]
+                                +'</a>'
+                                +'</div>');
+                                if(i == vrlist.length-1)
+                                    jq(el).append('<div class="item">'+'<a href=\'map.html\' return false;\'>지도로 돌아가기</a>'+'</div>')
+            }
+        });
+        
+    }
+
+    function init() {
+        generateContent();
+        jq('.scrollpanel').scrollpanel();
+    }
+    jq(init);
+}());
---
+++ js/vrlist_object34.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+
+    var win = window; // eslint-disable-line no-undef
+    var jq = win.jQuery;
+    var appendContent = true;
+    var vrlist = [ '','외관'];
+    
+    function generateContent() {
+        jq('.scrollpanel').each(function (idx, el) {
+            for (var i = 1; i < vrlist.length; i += 1) {
+                jq(el).append('<div class="item">'
+                                +'<a target="demo" href="vr34_'
+                                +i+'.html">'
+                                +vrlist[i]
+                                +'</a>'
+                                +'</div>');
+                                if(i == vrlist.length-1)
+                                    jq(el).append('<div class="item">'+'<a href=\'map.html\' return false;\'>지도로 돌아가기</a>'+'</div>')
+            }
+        });
+        
+    }
+
+    function init() {
+        generateContent();
+        jq('.scrollpanel').scrollpanel();
+    }
+    jq(init);
+}());
